20da9c8c927e50105e862d400e400b86e8a95bf8
ralfz123 <ralf_zonneveld@hotmail.com>
2020-03-02 20:24:12 +0100

    activates template ejs and the app can render dynamic data

--- index.js
+++ index.js
@@ -5,13 +5,47 @@ const express = require ('express');
 const app = express();
 const port = 3000;
 const path = require('path');
-// const views = require ('views');
 
+// const data = {
+//    naam : 'Ralf',
+//    leeftijd: '20',
+//    woonplaats : 'Nieuw-Vennep'
+// }
+
+const movies = [ {
+    id : 'catch-me-if-you-can',
+    title : 'Catch me if you can',
+    plot: 'A seasoned FBI agent pursues Frank Abagnale Jr. who, before his 19th birthday, successfully forged millions of dollars worth of checks while posing as a Pan Am pilot, a doctor, and a legal prosecutor. New Rochelle, the 1960s.' ,
+    description : 'before his 19th birthday.',
+},
+
+{
+    id : 'knives-out',
+    title : 'Knives out',
+    plot : 'A detective investigates the death of a patriarch of an eccentric, combative family.',
+    description : 'Writer Harlan Thrombey is found dead in his house, just after his 85th birthday. Detective Benoit Blanc is called in to solve the mystery, which is quite a challenge. Harlan has rather strange and suspicious family members and also a number of staff members who served him when he was alive.',
+},
+
+{
+    id : 'batman',
+    title : 'batman',
+    plot : 'A detective investigates the death of a patriarch of an eccentric, combative family.',
+    description : 'Writer Harlan Thrombey is found dead in his house, just after his 85th birthday. Detective Benoit Blanc is called in to solve the mystery, which is quite a challenge. Harlan has rather strange and suspicious family members and also a number of staff members who served him when he was alive.',
+}
+]
+
+
+
+//Locate ejs (and views)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // Using static directory:
 app.use('/static', express.static('static'));
+
+// Render a page
+app.get('/', (req, res) => res.render('object.ejs', {data: movies}));
+
 
 
 // Getting home.html file:
@@ -26,17 +60,6 @@ app.get('/foto', (req, res) => res.sendFile(path.join(__dirname + '/static/logo-
 //Getting an mp3 file
 app.get('/mp3', (req, res) => res.sendFile(path.join(__dirname + '/static/Sabre_Dance_Aram_Khachaturian.mp3')));
 
-//Getting the pages from the application
-app.get('/home', (req, res) => res.sendFile(path.join(__dirname + '/static/Sabre_Dance_Aram_Khachaturian.mp3')));
-
-
-res.render('index')
-
-
-// query parameters exercise = ???
-app.get('/users/:userId/books/:bookId', (req, res) => res.send(req.params));
-
-
 // Getting about.html file:
 app.get('/about', (req, res) => res.send('De About-page'));
 
@@ -44,22 +67,21 @@ app.get('/about', (req, res) => res.send('De About-page'));
 app.get('*', (req, res) => res.send('De Error 404 pagina'));
 
 
+
+
+
+
+
+
+// query parameters exercise = ???
+// app.get('/users/:userId/books/:bookId', (req, res) => res.send(req.params));
+
+
 // Server is listening on port:
 app.listen(port, () => console.log('listening on port ' + port));
 
-//route to index.ejs
-app.get('/', (req, res) => {
-  res.render('index', { title: 'Hey', message: 'Hello there!' })
-})
-
-
-app.get
 
 
 
 console.log(cc('LEUKE-OPDRACHT'));
-console.log('The route parameters are ' );
-
-
-
-// console.log(camelCase('foo-bar'));
+// console.log('The route parameters are ' );
