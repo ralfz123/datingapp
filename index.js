d6c444863bc85de174716ac618f2dc3ae02e30dc
ralfz123 <ralf_zonneveld@hotmail.com>
2020-03-17 14:28:11 +0100

    added includes to ejs files and removed useless lines

--- index.js
+++ index.js
@@ -29,31 +29,42 @@ mongo.MongoClient.connect(uri, function (err, client) {
 
 
 
-const movies = [ {
-    id : 'catch-me-if-you-can',
-    title : 'Catch me if you can',
-    plot: 'A seasoned FBI agent pursues Frank Abagnale Jr. who, before his 19th birthday, successfully forged millions of dollars worth of checks while posing as a Pan Am pilot, a doctor, and a legal prosecutor. New Rochelle, the 1960s.' ,
-    description : 'before his 19th birthday.',
-},
+// const movies = [ {
+//     id : 'catch-me-if-you-can',
+//     title : 'Catch me if you can',
+//     plot: 'A seasoned FBI agent pursues Frank Abagnale Jr. who, before his 19th birthday, successfully forged millions of dollars worth of checks while posing as a Pan Am pilot, a doctor, and a legal prosecutor. New Rochelle, the 1960s.' ,
+//     description : 'before his 19th birthday.',
+// },
 
-{
-    id : 'knives-out',
-    title : 'Knives out',
-    plot : 'A detective investigates the death of a patriarch of an eccentric, combative family.',
-    description : 'Writer Harlan Thrombey is found dead in his house, just after his 85th birthday. Detective Benoit Blanc is called in to solve the mystery, which is quite a challenge. Harlan has rather strange and suspicious family members and also a number of staff members who served him when he was alive.',
-},
+// {
+//     id : 'knives-out',
+//     title : 'Knives out',
+//     plot : 'A detective investigates the death of a patriarch of an eccentric, combative family.',
+//     description : 'Writer Harlan Thrombey is found dead in his house, just after his 85th birthday. Detective Benoit Blanc is called in to solve the mystery, which is quite a challenge. Harlan has rather strange and suspicious family members and also a number of staff members who served him when he was alive.',
+// },
 
-{
-    id : 'batman',
-    title : 'batman',
-    plot : 'A detective investigates the death of a patriarch of an eccentric, combative family.',
-    description : 'Writer Harlan Thrombey is found dead in his house, just after his 85th birthday. Detective Benoit Blanc is called in to solve the mystery, which is quite a challenge. Harlan has rather strange and suspicious family members and also a number of staff members who served him when he was alive.',
-}
-]
+// {
+//     id : 'batman',
+//     title : 'batman',
+//     plot : 'A detective investigates the death of a patriarch of an eccentric, combative family.',
+//     description : 'Writer Harlan Thrombey is found dead in his house, just after his 85th birthday. Detective Benoit Blanc is called in to solve the mystery, which is quite a challenge. Harlan has rather strange and suspicious family members and also a number of staff members who served him when he was alive.',
+// }
+// ]
 
-//Locate ejs (and views)
+// Using static files from static directory:
+app.use('/static', express.static('static'));
+
+//Locate ejs(templating) (and views) 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+// routing 
+
+
+
+
+
+
 
 // app.use(bodyParser.urlencoded({extended: true}));
 app.use(urlencodedParser);
@@ -66,8 +77,7 @@ app.use(session({
 
 app.get('/log-out', logout);
 
-// Using static directory:
-app.use('/static', express.static('static'));
+
 
 // get the id
 app.get('/:id', movie)
@@ -82,15 +92,6 @@ app.get('/add', form);
 
 // Getting home.html file:
 // app.get('/home', (req, res) => res.send('De Homepage'));
-
-// Getting contact.html file:
-app.get('/contact', (req, res) => res.sendFile(path.join(__dirname + '/static/contact.html')));
-
-//Getting an image
-app.get('/foto', (req, res) => res.sendFile(path.join(__dirname + '/static/logo-techv2.png')));
-
-//Getting an mp3 file
-app.get('/mp3', (req, res) => res.sendFile(path.join(__dirname + '/static/Sabre_Dance_Aram_Khachaturian.mp3')));
 
 // Getting about.html file:
 app.get('/about', (req, res) => res.send('De About-page'));
