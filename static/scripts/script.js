59daf67e179b2f0fd05dba12fb5aca63aecf533d
ralfz123 <ralf_zonneveld@hotmail.com>
2020-03-27 10:20:01 +0100

    multiform works better than first

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -29,6 +29,8 @@
 // Beschrijven wat er gebeurd
 var fieldset = document.getElementsByTagName('fieldset');
 var button = document.getElementsByTagName('button');
+// var buttonOne = document.getElementsByClassName("back");
+// var buttonTwo = document.getElementsByClassName("next");
 var li = document.getElementsByTagName('li');
 var form = document.getElementsByTagName('form');
 form.onsubmit =()=>{return false};
@@ -49,17 +51,17 @@ button[1].onclick = ()=>{
     current_fieldset++;
     var back_fieldset = current_fieldset - 1;
     if ((current_fieldset > 0) && (current_fieldset < 3)){
-        button[0].style.display = "inline";
-        fieldset[current_fieldset].style.display = "block";
-        fieldset[back_fieldset].style.display = "none";
-        li[current_fieldset].style.backgroundColor = "red";
-        li[back_fieldset].style.backgroundColor = "#b3b3b3";
-        if (current_fieldset == 4){
-            button[1].innerHTML = "Submit";
-        }
+            button[1].style.display = "inline";
+            fieldset[current_fieldset].style.display = "block";
+            fieldset[back_fieldset].style.display = "none";
+            li[current_fieldset].style.backgroundColor = "red";
+            li[back_fieldset].style.backgroundColor = "#b3b3b3";
+             if (current_fieldset == 2){
+                 button[1].innerHTML = "Submit";
+             }
  
     } else {
-        if (current_fieldset > 3){
+        if (current_fieldset > 2){
             form.onsubmit =()=>{return true};
         }
     }
@@ -68,21 +70,23 @@ button[1].onclick = ()=>{
 
 button[0].onclick = ()=>{
     if (current_fieldset > 0){
-        current_fieldset--;
-        var back_fieldset = current_fieldset + 1;
-        fieldset[current_fieldset].style.display = "block";
-        fieldset[back_fieldset].style.display = "none";
-        li[current_fieldset].style.backgroundColor = "red";
-        li[back_fieldset].style.backgroundColor = "#b3b3b3";
-        if (current_fieldset < 4){
-            button[1].innerHTML = "Verder";
-        }
+             current_fieldset--;
+             var back_fieldset = current_fieldset + 1;
+             fieldset[current_fieldset].style.display = "block";
+             fieldset[back_fieldset].style.display = "none";
+             li[current_fieldset].style.backgroundColor = "red";
+             li[back_fieldset].style.backgroundColor = "#b3b3b3";
+             button[1].innerHTML = "Verder";
+                // if (current_fieldset < 4){
+             //     // button[1].innerHTML = "Verder";
+             // }
 
     }
     if (current_fieldset == 0){
         // button[0].style.display = "none";
-        location.href = ('/');
+        
     }
+    // if (current_fieldset <= 0){
+    //     location.href = ('/');
+    // }
 }
-
-// location.href = ('../../views/index.ejs');
