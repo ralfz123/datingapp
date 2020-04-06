7b766912f85268943b8342660fa9c0b3d69d930d
ralfz123 <ralf_zonneveld@hotmail.com>
2020-04-06 02:14:53 +0200

    Changed the server structure and added comments

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//import express packages
+// Import express packages
 const express = require('express');
 const app = express();
 const port = 8000;
@@ -34,7 +34,8 @@ mongo.MongoClient.connect(uri, function(err, client) {
 
 
 
-// Middelware set-up:
+// Middelware set-up
+
 // Using static files from static directory:
 app.use('/static', express.static('static'));
 
@@ -43,6 +44,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(urlencodedParser);
+// Session
 app.use(
     session({
         secret: process.env.SESSION_SECRET,
@@ -57,94 +59,23 @@ app.use(
 app.get('/', function(req, res) {
     res.render('index.ejs')
 });
-app.get('/profile', function(req, res) {
-    console.log(req.session.userId);
-    db.collection('users')
-        .findOne({
-            _id : ObjectID(req.session.userId)
-    })
-    .then(data=>{
-        res.render('profile.ejs' , {data});
-    })
-    .catch(err=>{
-        console.log(err);
-        res.redirect('404error');
-    });
-});
-
-
 app.get('/registreer_p1', function(req, res) {
     res.render('registreer_p1.ejs');
 });
 app.get('/registreer_p4', function(req, res) {
     res.render('registreer_p4.ejs');
 });
+app.get('/profile', profile);
 app.post('/', login);
 app.post('/registrate', upload.single('photo'), urlencodedParser, makeUser);
-app.post('/logout', function (req, res) {
-    req.session.destroy();
-    res.redirect('/');
-    console.log("Uitgelogd!")
-});
+app.post('/logout', logout);
 app.post('/profile', editProfile);
-app.get('*', function(req, res) {
-    res.render('404error');
-});
-
-function editProfile (req, res) {
-    const query = {  _id : ObjectID(req.session.userId)};
-    console.log(req.session.userId)
-    // console.log(data)
-        const updatedValues = {
-                $set: {
-                 'firstName' : req.body.firstName,
-                 'gender' : req.body.gender,
-                 'searchSex' : req.body.searchSex,
-                 'age' : req.body.age,
-                 'hometown' : req.body.hometown,
-                 'email' : req.body.email,
-                 'password' : req.body.password,
-                 'photo' : req.body.photo
-        }
-    }
-    console.log(updatedValues);
-    db.collection('users')
-        .findOneAndUpdate(query, updatedValues)
-        .then(data => {
-           console.log('heeft data gevonden');
-           console.log(query);
-           console.log(data);
-             if (data){
-                //  console.log(data)
-                 res.redirect('/profile');
-            }
-        })
-        .catch(err =>{
-            console.log(err)
-    })
-}
+app.get('*', error);
 
 
-function login (req, res){
-    db.collection('users')
-        .findOne({
-            firstName: req.body.firstName,
-            password: req.body.password
-    })
-    .then(data=> {
-            console.log('Ingelogd als ' + data.firstName);
-            req.session.userId = data._id;
-            if (data){
-                res.redirect('/profile');
-        }
-    })
-    .catch(err=>{
-        console.log(err);
-        res.redirect('404error');
-    });
-}
+// Functions
 
-// Register and the app makes an user in the DB
+// Register an account (inserted in DB)
 function makeUser(req, res) {
     let firstName = req.body.firstName;
     let gender = req.body.gender;
@@ -177,5 +108,92 @@ function makeUser(req, res) {
     });
 }
 
-// Server is listening on port:
+
+// Logging in
+function login (req, res){
+    db.collection('users')
+        .findOne({
+            firstName: req.body.firstName,
+            password: req.body.password
+    })
+    .then(data=> {
+            console.log('Ingelogd als ' + data.firstName);
+            req.session.userId = data._id;
+            if (data){
+                res.redirect('/profile');
+        }
+    })
+    .catch(err=>{
+        console.log(err);
+        res.redirect('404error');
+    });
+}
+
+
+// Logout
+function logout (req, res) {
+    req.session.destroy();
+    res.redirect('/');
+    console.log("Uitgelogd!")
+}
+
+
+// Profile page
+function profile(req, res) {
+    console.log(req.session.userId);
+    db.collection('users')
+        .findOne({
+            _id : ObjectID(req.session.userId)
+    })
+    .then(data=>{
+        res.render('profile.ejs' , {data});
+    })
+    .catch(err=>{
+        console.log(err);
+        res.redirect('404error');
+    });
+}
+
+
+// Update profile page
+function editProfile (req, res) {
+    const query = {  _id : ObjectID(req.session.userId)}; // the current user
+    console.log(req.session.userId)
+    const updatedValues = { // the new data values
+        $set: {
+            'firstName' : req.body.firstName,
+            'gender' : req.body.gender,
+            'searchSex' : req.body.searchSex,
+            'age' : req.body.age,
+            'hometown' : req.body.hometown,
+            'email' : req.body.email,
+            'password' : req.body.password,
+            'photo' : req.body.photo
+        }
+    }
+    console.log(updatedValues);
+
+
+    db.collection('users')
+        .findOneAndUpdate(query, updatedValues)
+
+        .then(data => {
+           console.log('heeft data gevonden');
+           console.log(query);
+           console.log(data);
+             if (data){
+                 res.redirect('/profile'); // profile with updated data
+            }
+        })
+        .catch(err =>{
+            console.log(err)
+    })
+}
+
+// Error
+function error(req, res) {
+    res.render('404error');
+};
+
+// Server is listening on port
 app.listen(port, () => console.log('listening on port ' + port));
